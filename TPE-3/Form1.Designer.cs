fe69d0d41f0e97c85f68f1d8621eea9ec439885c
shaaydar <shaaydar@yandex.ru>
2022-03-21 23:17:27 +0500

    Добавьте файлы проекта.

---
+++ TPE-3/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TPE_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.nudVar = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rbLatin = new System.Windows.Forms.RadioButton();
+            this.rbYouden = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudVar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToOrderColumns = true;
+            this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.grid.BackgroundColor = System.Drawing.Color.White;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid.DefaultCellStyle = dataGridViewCellStyle2;
+            this.grid.Location = new System.Drawing.Point(12, 42);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.RowHeadersWidth = 60;
+            this.grid.Size = new System.Drawing.Size(597, 396);
+            this.grid.TabIndex = 0;
+            // 
+            // nudVar
+            // 
+            this.nudVar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudVar.Location = new System.Drawing.Point(647, 98);
+            this.nudVar.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudVar.Name = "nudVar";
+            this.nudVar.Size = new System.Drawing.Size(103, 20);
+            this.nudVar.TabIndex = 2;
+            this.nudVar.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Варьирований";
+            // 
+            // btnApply
+            // 
+            this.btnApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnApply.Location = new System.Drawing.Point(647, 415);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(122, 23);
+            this.btnApply.TabIndex = 4;
+            this.btnApply.Text = "Построить";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(72, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(537, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "x2";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(12, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "x1";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rbLatin
+            // 
+            this.rbLatin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.rbLatin.AutoSize = true;
+            this.rbLatin.Checked = true;
+            this.rbLatin.Location = new System.Drawing.Point(647, 219);
+            this.rbLatin.Name = "rbLatin";
+            this.rbLatin.Size = new System.Drawing.Size(122, 17);
+            this.rbLatin.TabIndex = 6;
+            this.rbLatin.TabStop = true;
+            this.rbLatin.Text = "латинский квадрат";
+            this.rbLatin.UseVisualStyleBackColor = true;
+            // 
+            // rbYouden
+            // 
+            this.rbYouden.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.rbYouden.AutoSize = true;
+            this.rbYouden.Location = new System.Drawing.Point(647, 242);
+            this.rbYouden.Name = "rbYouden";
+            this.rbYouden.Size = new System.Drawing.Size(102, 17);
+            this.rbYouden.TabIndex = 6;
+            this.rbYouden.Text = "квадрат Юдена";
+            this.rbYouden.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbYouden);
+            this.Controls.Add(this.rbLatin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudVar);
+            this.Controls.Add(this.grid);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "ТПЭ 3";
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudVar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.NumericUpDown nudVar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbLatin;
+        private System.Windows.Forms.RadioButton rbYouden;
+    }
+}
+
---
+++ TPE-3/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPE_3
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void btnApply_Click(object sender, EventArgs e)
+        {
+            var v = (int)nudVar.Value;
+            var thirdFactor = LatinSquare(v);
+            CreateTable(thirdFactor);
+
+            if (rbYouden.Checked)
+            {
+                // скрываем случайный столбец/строку
+                var isRow = random.Next(2) > 0;
+                var position = random.Next(v);
+
+                if (isRow)
+                    grid.Rows[position].Visible = false;
+                else
+                    grid.Columns[position].Visible = false;
+            }
+        }
+
+        List<List<int>> LatinSquare(int v)
+        {
+            var model = new List<List<int>>();
+            int c;
+            for (int i = 0; i < v; i++)
+            {
+                model.Add(new List<int>());
+                for (int j = 0; j < v; j++)
+                {
+                    c = (i + j) % v;
+                    model[i].Add(c);
+                }
+            }
+
+            return model;
+        }
+
+        double GetVaried(int elem, int full)
+        {
+            var part = 2.0 / (full - 1);
+            return -1 + (part * elem);
+        }
+
+        void CreateTable(List<List<int>> model)
+        {
+            grid.Rows.Clear();
+            grid.Columns.Clear();
+
+            string name;
+            var v = model.Count;
+            for (int col = 0; col < v; col++)
+            {
+                name = GetVaried(col, v).ToString("f2");
+                grid.Columns.Add(name, name);
+
+                for (int row = 0; row < model.Count; row++)
+                {
+                    if (col == 0)
+                    {
+                        grid.Rows.Add(new DataGridViewRow()
+                        {
+                            HeaderCell = new DataGridViewRowHeaderCell() { Value = GetVaried(row, v).ToString("f2") }
+                        });
+                    }
+
+                    grid.Rows[row].Cells[col].Value = 
+                        $"x3 = {GetVaried(model[row][col], v).ToString("f2")}{Environment.NewLine}y[{row+1},{col+1},{model[row][col]+1}]";
+                }
+            }
+
+        }
+
+    }
+}
